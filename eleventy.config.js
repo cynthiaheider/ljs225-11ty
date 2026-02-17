60957446df619a3e2a5f3a379499e70436677f34
Cynthia <15270811+cynthiaheider@users.noreply.github.com>
2026-02-17 17:27:04 -0500

    move the images again and add passthrough

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("ljs225-combined");
 };
